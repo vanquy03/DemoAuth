8dbd36b2a53af9fde2e8cfe6f3356d48aef5d620
vanquy03 <quyvan2003@gmail.com>
2025-09-27 21:44:38 +0700

    push on test

--- WebServer/Controllers/AuthController.cs
+++ WebServer/Controllers/AuthController.cs
@@ -10,7 +10,7 @@ using Microsoft.IdentityModel.Tokens;
 namespace WebServer.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[backend]/[controller]")]
     public class AuthController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
